646e181c819477689255fa72fc0ea1a78cb46a71
carlos gutierrez <gosu2424@gmail.com>
2018-03-27 16:39:23 -0600

    first test commit

---
+++ AxonifyIntegration/AxonifyIntegration.Database/API_IntroductoryCompletions_1.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[API_IntroductoryCompletions]
+(
+	[employeeId] NVARCHAR(255) NOT NULL PRIMARY KEY, 
+    [categoryExternalId] NVARCHAR(60) NULL, 
+    [subjectExternalId] NVARCHAR(60) NULL, 
+    [topicExternalId] NVARCHAR(60) NULL, 
+    [level] INT NULL, 
+    [completionTimestamp] DATETIME NULL, 
+    [assessmentScore] DECIMAL(2, 1) NULL, 
+    [timeSpent] INT NULL
+)
