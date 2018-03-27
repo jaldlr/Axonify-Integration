0245ef2d321e28d091ea1d584456587d87d279a1
jaldlr <jorge.jaldlr@gmail.com>
2018-03-26 22:24:41 -0600

    Were created all layers in the project

---
+++ AxonifyIntegration/AxonifyIntegration.WebApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace AxonifyIntegration.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ AxonifyIntegration/AxonifyIntegration.WebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AxonifyIntegration.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
