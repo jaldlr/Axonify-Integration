0b69aa37c71548ed69b4781f5bd9dd8a114f07b7
carlos gutierrez <gosu2424@gmail.com>
2018-03-27 17:35:04 -0600

    add new catalog tables

---
+++ AxonifyIntegration/AxonifyIntegration.Database/Tables/API_Course.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[API_Course]
+(
+	[courseCode] NVARCHAR(53) NOT NULL PRIMARY KEY, 
+    [courseName] NVARCHAR(177) NOT NULL
+)
---
+++ AxonifyIntegration/AxonifyIntegration.Database/Tables/API_Topic.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[API_Topic]
+(
+	[topicExternalId] NVARCHAR(128) NOT NULL PRIMARY KEY, 
+    [subjectExternalId] NVARCHAR(128) NOT NULL, 
+    [topicName] NVARCHAR(60) NULL, 
+    [revision] NVARCHAR(128) NOT NULL
+)
