e39a49bcffb497dc256157f508876164f3d28857
carlos gutierrez <gosu2424@gmail.com>
2018-03-28 11:22:21 -0600

    check in more new tables

---
+++ AxonifyIntegration/AxonifyIntegration.Database/Tables/API_KnowladgeMap.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[API_KnowledgeMap]
+(
+	[knowledgeMapId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [employeeId] NVARCHAR(255) NULL, 
+    [categoryExternalId] NVARCHAR(60) NULL, 
+    [subjectExternalId] NVARCHAR(60) NULL, 
+    [topicExternalId] NVARCHAR(60) NULL, 
+    [learnerStatusId] INT NULL, 
+    [certificationStatusId] INT NULL, 
+    [introductoryStatusId] INT NULL, 
+    [trainingModuleCompletedId] INT NULL, 
+    [trainingModuleLastCompletionTimestamp] DATETIME NULL, 
+    [topicGraduationTimestamp] DATETIME NULL 
+)
