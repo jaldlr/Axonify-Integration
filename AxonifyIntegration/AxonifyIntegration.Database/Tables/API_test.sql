752c93f9ab418f68807f0efb7b974fe6840d6561
murdoksc <37823597+murdoksc@users.noreply.github.com>
2018-03-27 19:43:04 -0600

    testcheck in from desktop
    test check with our vs profesional

---
+++ AxonifyIntegration/AxonifyIntegration.Database/Tables/API_test.sql
@@ -0,0 +1,16 @@
+USE [axonify]
+GO
+
+/****** Object:  Table [dbo].[test]    Script Date: 27/03/2018 07:29:55 p. m. ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[test](
+	[testtest] [nchar](10) NULL
+) ON [PRIMARY]
+GO
+
+
