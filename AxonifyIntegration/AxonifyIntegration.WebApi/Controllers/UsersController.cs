352a6dc7a36c3ec1e06fcdda76d7f326cba111d5
gdljorlu <jorge.jaldlr@gmail.com>
2018-03-29 10:54:31 -0600

    Was created AxonifyAuthorization class

---
+++ AxonifyIntegration/AxonifyIntegration.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using AxonifyIntegration.WebApi.Utilities;
+
+namespace AxonifyIntegration.WebApi.Controllers
+{
+    [AxonifyAuthorization]
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Content("Hola mundo");
+        }
+
+        public ActionResult Get()
+        {
+            return Content("Hola mundo 2");
+        }
+    }
+}
---
+++ AxonifyIntegration/AxonifyIntegration.WebApi/Utilities/AxonifyAuthorization.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AxonifyIntegration.WebApi.Utilities
+{
+    public class AxonifyAuthorization : System.Web.Mvc.AuthorizeAttribute
+    {
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            return true;
+        }
+    }
+}
