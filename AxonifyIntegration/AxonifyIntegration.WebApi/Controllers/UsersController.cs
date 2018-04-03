276fea145b592928cef80d10a07e56d06fc9362e
jaldlr <jorge.jaldlr@gmail.com>
2018-04-02 22:45:17 -0500

    Were created apis for Areas of Interest

--- AxonifyIntegration/AxonifyIntegration.WebApi/Controllers/UsersController.cs
+++ AxonifyIntegration/AxonifyIntegration.WebApi/Controllers/UsersController.cs
@@ -32,10 +32,10 @@ namespace AxonifyIntegration.WebApi.Controllers
         #endregion
 
         #region Api Methods
-
-        // GET: Users
+        
         [HttpGet]
-        public HttpResponseMessage Get()
+        [Route("users")]
+        public HttpResponseMessage GetAll()
         {
             HttpResponseMessage response;
 
@@ -55,10 +55,10 @@ namespace AxonifyIntegration.WebApi.Controllers
 
             return response;
         }
-
-        // Put: Users
+        
         [HttpPut]
-        public HttpResponseMessage Put(List<API_User> users)
+        [Route("users")]
+        public HttpResponseMessage AddUsers(List<API_User> users)
         {
             HttpResponseMessage response;
             Boolean isValid = true;
@@ -133,6 +133,102 @@ namespace AxonifyIntegration.WebApi.Controllers
                 {
                     response = HttpStatusCodes.statusCode422;
                 }
+            }
+            catch (Exception ex)
+            {
+                response = HttpStatusCodes.statusCode500;
+            }
+
+            return response;
+        }
+
+        [HttpGet]
+        [Route("users/{employee_id}/aois")]
+        public HttpResponseMessage GetAreasOfInteres(string employee_id)
+        {
+            HttpResponseMessage response;
+
+            try
+            {
+                API_User user = (from s in this._dbContext.API_User where s.employeeId.Equals(employee_id, StringComparison.OrdinalIgnoreCase) select s).FirstOrDefault();
+                List<string> areasOfInterest = new List<string>();
+                if(user != null)
+                {
+                    areasOfInterest = (from t in user.API_UserAreasOfInterest select t.areaOfInterest).ToList();
+                }
+                var result = new
+                {
+                    areasOfInterests = areasOfInterest
+                };
+                response = Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                response = HttpStatusCodes.statusCode500;
+            }
+
+            return response;
+        }
+
+        [HttpPut]
+        [Route("users/{employee_id}/aois/{aoi_to_add}")]
+        public HttpResponseMessage AddAreaOfInteres(string employee_id, string aoi_to_add)
+        {
+            HttpResponseMessage response;
+
+            try
+            {
+                API_User user = (from s in this._dbContext.API_User where s.employeeId.Equals(employee_id, StringComparison.OrdinalIgnoreCase) select s).FirstOrDefault();
+
+                if (user != null && !string.IsNullOrEmpty(aoi_to_add) && (from t in user.API_UserAreasOfInterest where t.areaOfInterest.Equals(aoi_to_add) select t).FirstOrDefault() == null)
+                {
+                    user.API_UserAreasOfInterest.Add(new API_UserAreasOfInterest()
+                    {
+                        employeeId = user.employeeId,
+                        areaOfInterest = aoi_to_add
+                    });
+                    this._dbContext.SaveChanges();
+                }
+
+                var result = new
+                {
+                    status = "OK"
+                };
+                response = Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                response = HttpStatusCodes.statusCode500;
+            }
+
+            return response;
+        }
+
+        [HttpDelete]
+        [Route("users/{employee_id}/aois/{aoi_to_delete}")]
+        public HttpResponseMessage RemoveAreaOfInteres(string employee_id, string aoi_to_delete)
+        {
+            HttpResponseMessage response;
+
+            try
+            {
+                API_UserAreasOfInterest area = (
+                    from s in this._dbContext.API_UserAreasOfInterest
+                    where s.employeeId.Equals(employee_id, StringComparison.OrdinalIgnoreCase) && s.areaOfInterest.Equals(aoi_to_delete, StringComparison.OrdinalIgnoreCase)
+                    select s
+                ).FirstOrDefault();
+
+                if (area != null)
+                {
+                    this._dbContext.API_UserAreasOfInterest.Remove(area);
+                    this._dbContext.SaveChanges();
+                }
+
+                var result = new
+                {
+                    status = "OK"
+                };
+                response = Request.CreateResponse(HttpStatusCode.OK, result);
             }
             catch (Exception ex)
             {
